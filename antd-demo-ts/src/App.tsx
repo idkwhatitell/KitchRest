b02a8c728300506a11a24127b5a7104776c1afae
Kaan Cömert <comert9806@gmail.com>
2022-10-31 00:59:56 +0300

    Table integration to layout

--- antd-demo-ts/src/App.tsx
+++ antd-demo-ts/src/App.tsx
@@ -10,6 +10,10 @@ import {
 import type { MenuProps } from 'antd';
 import { Breadcrumb, Layout, Menu } from 'antd';
 import React, { useState } from 'react';
+import DataTable from './Datatable';
+
+
+
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -38,7 +42,7 @@ const items: MenuItem[] = [
     getItem('Alex', '5'),
   ]),
   getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('Files', '9', <FileOutlined />),
+  
 ];
 
 const App: React.FC = () => {
@@ -51,17 +55,18 @@ const App: React.FC = () => {
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
       </Sider>
       <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Header className="site-layout-background" style={{ padding: 5 }} />
         <Content style={{ margin: '0 16px' }}>
           <Breadcrumb style={{ margin: '16px 0' }}>
             <Breadcrumb.Item>User</Breadcrumb.Item>
             <Breadcrumb.Item>Bill</Breadcrumb.Item>
           </Breadcrumb>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          <div className="site-layout-background" style={{ padding: 5, minHeight: 360 }}>
+            <DataTable/>
             Bill is a cat.
           </div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        <Footer style={{ textAlign: 'center' }}>Kaan Cömert ©2022 Created for KitchRest</Footer>
       </Layout>
     </Layout>
   );
